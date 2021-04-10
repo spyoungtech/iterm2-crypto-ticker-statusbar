d68faa94fa6eb572d18bafbf090aeec40bf99ab4
Spencer Young <spencer.young@spyoung.com>
2021-04-09 19:08:04 -0700

    single ticker per component

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # iTerm2 Cryto ticker
 
-Cryto ticker for your iterm status bar
+Cryto ticker for your iterm status bar. Backed by ethereumdb. Supports multiple coins and currencies.
 
-![screenshot](https://user-images.githubusercontent.com/15212758/114246667-07a6d080-9948-11eb-934b-45889d111089.png)
+![screenshot](https://user-images.githubusercontent.com/15212758/114254794-7561f500-9966-11eb-90a3-8e4bf579294d.png)
 
 
 
@@ -14,13 +14,20 @@ For more information see the [iterm2 documentation](https://iterm2.com/documenta
 
 ## Installing the script
 
-1. Open iTerm2. In the menu, click **Scripts** > **Manage** > **New Python Script**
-2. When prompted, select **Full Environment** then select **Long-Running Daemon**
-3. Specify the **Save As** name `cryptoticker` and **PyPI Dependencies** as `aiohttp`. If you haven't downloaded a python runtime for iTerm, you'll be prompted to do so.
-4. Copy the cryptoticker.py to the iterm environment:  
-`cp ./cryptoticker.py ~/Library/Application Support/iTerm2/Scripts/cryptoticker/cryptoticker/cryptoticker.py`
+Open iTerm2. In the menu, click **Scripts** > **Manage** > **New Python Script**
+
+When prompted, select **Full Environment** then select **Long-Running Daemon**
+
+Specify the **Save As** name `cryptoticker` and **PyPI Dependencies** as `aiohttp`. If you haven't downloaded a python runtime for iTerm, you'll be prompted to do so.
+
+Copy the cryptoticker.py to the iterm environment:  
+```  
+cp ./cryptoticker.py ~/Library/Application Support/iTerm2/Scripts/cryptoticker/cryptoticker/cryptoticker.py  
+```  
 5. Move cryptoticker into the AutoLaunch folder (create this directory if it does not exist)  
-`mv ~/Library/Application Support/iTerm2/Scripts/cryptoticker ~/Library/Application Support/iTerm2/Scripts/AutoLaunch/cryptoticker`
+```  
+mv ~/Library/Application Support/iTerm2/Scripts/cryptoticker ~/Library/Application Support/iTerm2/Scripts/AutoLaunch/cryptoticker
+```
 
 ## Configure the status bar
 
@@ -29,11 +36,10 @@ For more information see the [iterm2 documentation](https://iterm2.com/documenta
 Go to **Preferences** > **Profiles** > **Session**. Turn on **Status bar enabled**.
 2. To add the ticker component, click **Configure Status Bar**. Drag the cryto ticker component into the 
 active components area.
-3. Click on the component then **Configure Component**. Here you can enable/disable specific coins or specific data. 
-By default, all coins are enabled and only price is shown.
+3. Click on the component then **Configure Component**. Here you can enable specific data for the coin.
 
-![config](https://user-images.githubusercontent.com/15212758/114246735-33c25180-9948-11eb-85a9-d7b3771e65f7.png)
+![config](https://user-images.githubusercontent.com/15212758/114254807-90cd0000-9966-11eb-86ac-baccf89b3060.png)
 
 You can also configure by `ctrl+click`ing on the status bar itself.
 
-![quickconfig](https://user-images.githubusercontent.com/15212758/114246701-1f7e5480-9948-11eb-8cce-e8367d100e19.png)
+![quickconfig](https://user-images.githubusercontent.com/15212758/114254794-7561f500-9966-11eb-90a3-8e4bf579294d.png)
--- cryptoticker.py
+++ cryptoticker.py
@@ -3,14 +3,23 @@ import iterm2
 import aiohttp
 import time
 import random
+from collections import defaultdict
+import copy
+COIN_SYMBOLS = defaultdict(str)
 
-
-SYMBOLS = {
+COIN_SYMBOLS.update({
     "ETH": "⧫",
     "BCH": "₿c",
     "LTC": "Ł",
     "BTC": "₿",
-}
+})
+
+
+CURRENCY_SYMBOLS = defaultdict(lambda: "$")
+CURRENCY_SYMBOLS.update(copy.deepcopy(COIN_SYMBOLS))
+CURRENCY_SYMBOLS.update({'EUR': '€', 'JPY': '¥', 'GBP': '£', 'USD': '$'})
+
+
 
 UP = "▲"
 DOWN = "▼"
@@ -18,35 +27,33 @@ DOWN = "▼"
 UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774"
 
 
-async def get_price(coin):
-    if coin not in SYMBOLS:
-        raise ValueError(f"Coin must be one of {'|'.join(SYMBOLS.keys())}")
+async def get_price(coin, currency="USD"):
     async with aiohttp.ClientSession() as session:
         t = int(time.time()) - 1
         r = random.randint(100, 999)
-        eth_url = f'https://api.ethereumdb.com/v1/ticker?pair={coin}-USD&range=24h&t={t}{r}'
+        eth_url = f'https://api.ethereumdb.com/v1/ticker?pair={coin}-{currency}&range=24h&t={t}{r}'
         async with session.get(eth_url, headers={'User-Agent': UA}) as resp:
+            resp.raise_for_status()
             data = await resp.json()
-
     return data
 
 
 async def main(connection):
     # Define the configuration knobs:
-    coins = list(SYMBOLS.keys())
-    knobs = []
-    for coin in coins:
-        knob = iterm2.CheckboxKnob(coin, True, coin)
-        knobs.append(knob)
+    coins = list(COIN_SYMBOLS.keys())
+    knobs = [iterm2.StringKnob('Coin (e.g. "BTC")', "ETH", "ETH", "coin"),
+             iterm2.StringKnob('Currency (e.g. "USD")', "USD", "USD", "currency")
+             ]
 
     knobs.append(iterm2.CheckboxKnob('show price', True, 'price'))
+
     data_opts = ['high', 'low', 'change', 'changePercent', 'open']
     for opt in data_opts:
         knobs.append(iterm2.CheckboxKnob(f'show {opt}', False, opt))
     component = iterm2.StatusBarComponent(
         short_description="Show crypto tickers in the status bar",
-        detailed_description="Show the price, changes, and other data about various cryto coins in the status bar. "
-        f"Updates every 10 seconds. Supported coins are {' '.join(SYMBOLS)}",
+        detailed_description="Show the price, changes, and other data about a given cryto coins in the status bar. "
+        f"Updates every 10 seconds. Uses ethereumdb api",
         knobs=knobs,
         exemplar="Crypto Ticker",
         update_cadence=10,
@@ -55,52 +62,44 @@ async def main(connection):
 
     @iterm2.StatusBarRPC
     async def coro(knobs):
-        prices = []
-        for coin in coins:
-            if coin in knobs and knobs[coin]:
-                price_data = await get_price(coin)
-                prices.append(price_data)
+        if 'coin' in knobs:
+            coin = knobs['coin']
+        else:
+            coin = 'ETH'
 
+        if 'currency' in knobs:
+            currency = knobs['currency']
+        else:
+            currency = 'USD'
+        price_data = await get_price(coin, currency)
         # format the data according to settings
-        strings = []
-        for price_data in prices:
-            pair = price_data['pair']
-            coin = pair.split('-')[0]
+        pair = price_data['pair']
+        currency_symbol = CURRENCY_SYMBOLS.get(currency, '$')
 
-            s = f'{SYMBOLS[coin]}'
-            if 'price' in knobs and knobs['price']:
-                price = round(price_data['price'], 4)
-                s += f' {pair}: ${price}'
-            for opt in ['high', 'low', 'open']:
-                if opt in knobs and knobs[opt]:
-                    s += f' {opt}: ${price_data.get(opt, "ERR")}'
-            if ('change' in knobs and knobs['change']) or ('changePercent' in knobs and knobs['changePercent']):
-                if price_data['change'] >= 0:
-                    symbol = UP
-                else:
-                    symbol = DOWN
-                cs = f'{symbol}'
-                if 'change' in knobs and knobs['change']:
-                    change = round(price_data['change'], 3)
-                    cs += f' ${change}'
-                if 'changePercent' in knobs and knobs['changePercent']:
-                    change_percent = round(price_data['changePercent'], 4)
-                    cs += f' {change_percent}%'
-                s += f' {cs}'
+        s = f'{COIN_SYMBOLS[coin]}'
+        if 'price' in knobs and knobs['price']:
+            price = round(price_data['price'], 4)
+            s += f' {pair}: {currency_symbol}{price}'
+        for opt in ['high', 'low', 'open']:
+            if opt in knobs and knobs[opt]:
+                s += f' {opt}: {currency_symbol}{price_data.get(opt, "ERR")}'
+        if ('change' in knobs and knobs['change']) or ('changePercent' in knobs and knobs['changePercent']):
+            if price_data['change'] >= 0:
+                symbol = UP
+            else:
+                symbol = DOWN
+            cs = f'{symbol}'
+            if 'change' in knobs and knobs['change']:
+                change = round(price_data['change'], 3)
+                cs += f' ${change}'
+            if 'changePercent' in knobs and knobs['changePercent']:
+                change_percent = round(price_data['changePercent'], 4)
+                cs += f' {change_percent}%'
+            s += f' {cs}'
 
-            strings.append(s)
 
-        # Provide various sizes that will show/hide according to window width
-        rets = [' | '.join(strings), *strings]
-        for i in range(2, len(strings)):
-            group = strings[0:i]
-            rets.append(' | '.join(group))
-        without_pairs = ' | '.join(strings)
-        for p in prices:
-            without_pairs = without_pairs.replace(p['pair'], '')
-        rets.append(without_pairs)
-        rets.sort(key=len, reverse=True)
-        return rets
+        without_pair = s.replace(price_data['pair'], '')
+        return [s, without_pair]
 
     # Register the component.
     await component.async_register(connection, coro)
